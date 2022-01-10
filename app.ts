58d30a04d5484a1f706365549b143ecac9272f59
chongzixin <chongzixin@zis-macbook-pro.lan>
2022-01-10 08:45:23 +0800

    added grid gallery to index

--- app.ts
+++ app.ts
@@ -1,1 +1,28 @@
-console.log("Inside the log!")
+const testFolder = '../assets';
+
+const imgArr = [
+    'black.png',
+    'blue.png',
+    'green.png',
+    'orange.png',
+    'pink.png',
+    'purple.png',
+    'red.png',
+];
+
+
+// randomise array before loading into gallery
+// TODO: shuffle array
+const gallery = document.getElementById("gallery");
+imgArr.forEach(filename => {
+    // TODO: add index to the ID
+    gallery!.innerHTML += `<div class="grid-item"><img src="assets/${filename}"></div>`;
+});
+
+
+// get the element that is currently selected
+// register keyboard event listener
+// key up: if odd, index--, else dont move
+// key down: if even, index++, else dont move
+// key left: index-2
+// key right: index+2
