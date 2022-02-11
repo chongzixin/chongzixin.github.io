32ef13b15c86421b6d2a2fb24cbe26ad2f64b0d5
Zixin <zchong@paypal.com>
2022-02-11 20:39:23 +0800

    refactor: randomise images within page instead of reloading entire page

--- app.ts
+++ app.ts
@@ -1,33 +1,7 @@
-// TODO: automatically get images from file
-// const fs = require('fs');
-// const assets_folder = './public/assets/';
-// fs.readdirSync(assets_folder).forEach((file: any) => {
-//     console.log(file);
-// });
+/* INIT */
 
 // pre-load the list of available images in assets
 const all_available_images: string[] = [
-    // // colours
-    // 'black.png',
-    // 'blue.png',
-    // 'green.png',
-    // 'orange.png',
-    // 'pink.png',
-    // 'purple.png',
-    // 'brown.png',
-    // 'red.png',
-    // 'grey.png',
-    // 'white.png',
-    // // fruits
-    // 'kiwi.png',
-    // 'strawberry.png',
-    // // animals
-    // 'zebra.png',
-    // 'tiger.png',
-    // 'elephant.png',
-    // 'lion.png',
-    // 'walrus.png',
-    // 'dolphin.png',
     // gecko garage
     'rick-the-roller.png',
     'caroline-the-crane.png',
@@ -69,88 +43,100 @@ const all_available_images: string[] = [
     'icecream-truck-smoothie.png',
 ];
 
-// choose your desired number of images and rows. show all images by default.
-const NUM_IMAGES_TO_SHOW = all_available_images.length;
-const NUM_ROWS = 2;
-
+const NUM_IMAGES_TO_SHOW = all_available_images.length; // change num images accordingly, show all by default.
+const NUM_ROWS = 2; // change number of rows accordingly
 const num_cols = Math.ceil(NUM_IMAGES_TO_SHOW / NUM_ROWS);
 
+/* MAIN */
+let images_to_show = getRandomThumbnails(all_available_images, NUM_IMAGES_TO_SHOW);
+paintImagesOnScreen(images_to_show);
+addEventListeners();
+
+/* FUNCTION DEFINITIONS */
+
 // randomly retrieve desired number of thumbnails from list
-const images_to_show = getRandomThumbnails(all_available_images, NUM_IMAGES_TO_SHOW);
-
-// set number of rows and columns in CSS
-const gallery = document.getElementById("gallery") as HTMLDivElement;
-gallery.style.gridTemplateRows = `repeat(${NUM_ROWS}, 300px)`;
-gallery.style.gridTemplateColumns = `repeat(${num_cols}, 400px)`;
-
-images_to_show.forEach((filename: string, index: number) => {
-    // produce the following: <div id="${index}" class="grid-item"><img src="assets/${filename}"></div>
-
-    const gallery_item = document.createElement("div") as HTMLDivElement;
-    gallery_item.id = (index + 1).toString();  // add 1 to the index to process our calculation
-    gallery_item.className = "grid-item";
-
-    // if this is the first image, set it as active
-    if(index === 0) {
-        gallery_item.className += " active";
-    }
-    
-    const image = document.createElement("img") as HTMLImageElement;
-    image.src = `assets/${filename}`;
-    gallery_item.appendChild(image);
-
-    gallery.appendChild(gallery_item);
-});
-
-window.onkeydown = (ev: KeyboardEvent): any => {
-    if(ev.defaultPrevented) return;
-    processKey(ev.key);
-
-    function processKey(key: string) {
-        const currentIndex = +document.getElementsByClassName("active")[0].id;
-        let newIndex;
-        
-        if(key === "ArrowUp" && currentIndex - num_cols > 0) {
-            newIndex = currentIndex - num_cols;
-            toggleActive(currentIndex, newIndex);
-        }
-        else if(key === "ArrowDown" && currentIndex + num_cols <= NUM_IMAGES_TO_SHOW) {
-            newIndex = currentIndex + num_cols;
-            toggleActive(currentIndex, newIndex);
-        }
-        else if(key === "ArrowLeft" && currentIndex % num_cols != 1) {
-            newIndex = currentIndex - 1;
-            toggleActive(currentIndex, newIndex);
-        }
-        else if(key === "ArrowRight" && currentIndex % num_cols != 0 && currentIndex + 1 <= NUM_IMAGES_TO_SHOW) {
-            newIndex = currentIndex + 1;
-            toggleActive(currentIndex, newIndex);
-        }
-        else if(key === "r") {
-            // if user presses R, refresh page to randomise images again.
-            location.reload();
-        }
-    }
-
-    function toggleActive(indexToRemove:number, indexToAdd:number) {
-        document.getElementById(indexToRemove.toString())?.classList.remove("active");
-        document.getElementById(indexToAdd.toString())?.classList.add("active");
-        document.getElementById(indexToAdd.toString())?.scrollIntoView();
-    }
-}
-
 function getRandomThumbnails(arr:string[], num:number):string[] {
     let result = new Array(num);
     let len = arr.length;
     let taken = new Array(len);
-
+    
     if (num > len)
-        throw new RangeError("getRandom: more elements taken than available");
+    throw new RangeError("getRandom: more elements taken than available");
     while (num--) {
         const x = Math.floor(Math.random() * len);
         result[num] = arr[x in taken ? taken[x] : x];
         taken[x] = --len in taken ? taken[len] : len;
     }
-
+    
     return result;
+}
+
+function paintImagesOnScreen(images:string[]) {
+    // set number of rows and columns in CSS
+    const gallery = document.getElementById("gallery") as HTMLDivElement;
+    gallery.innerHTML = ""; // clear the gallery before painting
+
+    gallery.style.gridTemplateRows = `repeat(${NUM_ROWS}, 300px)`;
+    gallery.style.gridTemplateColumns = `repeat(${num_cols}, 400px)`;
+    
+    images.forEach((filename: string, index: number) => {
+        // produce the following: <div id="${index}" class="grid-item"><img src="assets/${filename}"></div>
+        
+        const gallery_item = document.createElement("div") as HTMLDivElement;
+        gallery_item.id = (index + 1).toString();  // add 1 to the index to process our calculation
+        gallery_item.className = "grid-item";
+        
+        // if this is the first image, set it as active
+        if(index === 0) {
+            gallery_item.className += " active";
+        }
+        
+        const image = document.createElement("img") as HTMLImageElement;
+        image.src = `assets/${filename}`;
+        gallery_item.appendChild(image);
+        
+        gallery.appendChild(gallery_item);
+    });
+}
+
+function addEventListeners() {
+    window.onkeydown = (ev: KeyboardEvent): any => {
+        if(ev.defaultPrevented) return;
+        processKey(ev.key);
+        
+        function processKey(key: string) {
+            const currentIndex = +document.getElementsByClassName("active")[0].id;
+            let newIndex;
+            
+            if(key === "ArrowUp" && currentIndex - num_cols > 0) {
+                newIndex = currentIndex - num_cols;
+                toggleActive(currentIndex, newIndex);
+            }
+            else if(key === "ArrowDown" && currentIndex + num_cols <= NUM_IMAGES_TO_SHOW) {
+                newIndex = currentIndex + num_cols;
+                toggleActive(currentIndex, newIndex);
+            }
+            else if(key === "ArrowLeft" && currentIndex % num_cols != 1) {
+                newIndex = currentIndex - 1;
+                toggleActive(currentIndex, newIndex);
+            }
+            else if(key === "ArrowRight" && currentIndex % num_cols != 0 && currentIndex + 1 <= NUM_IMAGES_TO_SHOW) {
+                newIndex = currentIndex + 1;
+                toggleActive(currentIndex, newIndex);
+            }
+            else if(key === "r" || key === "End") {
+                // if user presses R or End, refresh page to randomise images again.
+                // we use End to provide convenience to users because it's near the arrow keys on the keyboard.
+                images_to_show = getRandomThumbnails(all_available_images, NUM_IMAGES_TO_SHOW);
+                paintImagesOnScreen(images_to_show);
+                toggleActive(currentIndex, 1);
+            }
+        }
+        
+        function toggleActive(indexToRemove:number, indexToAdd:number) {
+            document.getElementById(indexToRemove.toString())?.classList.remove("active");
+            document.getElementById(indexToAdd.toString())?.classList.add("active");
+            document.getElementById(indexToAdd.toString())?.scrollIntoView();
+        }
+    }
 }
--- public/dist/app.js
+++ public/dist/app.js
@@ -1,33 +1,7 @@
 "use strict";
-// TODO: automatically get images from file
-// const fs = require('fs');
-// const assets_folder = './public/assets/';
-// fs.readdirSync(assets_folder).forEach((file: any) => {
-//     console.log(file);
-// });
+/* INIT */
 // pre-load the list of available images in assets
 var all_available_images = [
-    // // colours
-    // 'black.png',
-    // 'blue.png',
-    // 'green.png',
-    // 'orange.png',
-    // 'pink.png',
-    // 'purple.png',
-    // 'brown.png',
-    // 'red.png',
-    // 'grey.png',
-    // 'white.png',
-    // // fruits
-    // 'kiwi.png',
-    // 'strawberry.png',
-    // // animals
-    // 'zebra.png',
-    // 'tiger.png',
-    // 'elephant.png',
-    // 'lion.png',
-    // 'walrus.png',
-    // 'dolphin.png',
     // gecko garage
     'rick-the-roller.png',
     'caroline-the-crane.png',
@@ -68,65 +42,15 @@ var all_available_images = [
     'sophie-larry-muddy.png',
     'icecream-truck-smoothie.png',
 ];
-// choose your desired number of images and rows. show all images by default.
-var NUM_IMAGES_TO_SHOW = all_available_images.length;
-var NUM_ROWS = 2;
+var NUM_IMAGES_TO_SHOW = all_available_images.length; // change num images accordingly, show all by default.
+var NUM_ROWS = 2; // change number of rows accordingly
 var num_cols = Math.ceil(NUM_IMAGES_TO_SHOW / NUM_ROWS);
-// randomly retrieve desired number of thumbnails from list
+/* MAIN */
 var images_to_show = getRandomThumbnails(all_available_images, NUM_IMAGES_TO_SHOW);
-// set number of rows and columns in CSS
-var gallery = document.getElementById("gallery");
-gallery.style.gridTemplateRows = "repeat(".concat(NUM_ROWS, ", 300px)");
-gallery.style.gridTemplateColumns = "repeat(".concat(num_cols, ", 400px)");
-images_to_show.forEach(function (filename, index) {
-    // produce the following: <div id="${index}" class="grid-item"><img src="assets/${filename}"></div>
-    var gallery_item = document.createElement("div");
-    gallery_item.id = (index + 1).toString(); // add 1 to the index to process our calculation
-    gallery_item.className = "grid-item";
-    // if this is the first image, set it as active
-    if (index === 0) {
-        gallery_item.className += " active";
-    }
-    var image = document.createElement("img");
-    image.src = "assets/".concat(filename);
-    gallery_item.appendChild(image);
-    gallery.appendChild(gallery_item);
-});
-window.onkeydown = function (ev) {
-    if (ev.defaultPrevented)
-        return;
-    processKey(ev.key);
-    function processKey(key) {
-        var currentIndex = +document.getElementsByClassName("active")[0].id;
-        var newIndex;
-        if (key === "ArrowUp" && currentIndex - num_cols > 0) {
-            newIndex = currentIndex - num_cols;
-            toggleActive(currentIndex, newIndex);
-        }
-        else if (key === "ArrowDown" && currentIndex + num_cols <= NUM_IMAGES_TO_SHOW) {
-            newIndex = currentIndex + num_cols;
-            toggleActive(currentIndex, newIndex);
-        }
-        else if (key === "ArrowLeft" && currentIndex % num_cols != 1) {
-            newIndex = currentIndex - 1;
-            toggleActive(currentIndex, newIndex);
-        }
-        else if (key === "ArrowRight" && currentIndex % num_cols != 0 && currentIndex + 1 <= NUM_IMAGES_TO_SHOW) {
-            newIndex = currentIndex + 1;
-            toggleActive(currentIndex, newIndex);
-        }
-        else if (key === "r") {
-            // if user presses R, refresh page to randomise images again.
-            location.reload();
-        }
-    }
-    function toggleActive(indexToRemove, indexToAdd) {
-        var _a, _b, _c;
-        (_a = document.getElementById(indexToRemove.toString())) === null || _a === void 0 ? void 0 : _a.classList.remove("active");
-        (_b = document.getElementById(indexToAdd.toString())) === null || _b === void 0 ? void 0 : _b.classList.add("active");
-        (_c = document.getElementById(indexToAdd.toString())) === null || _c === void 0 ? void 0 : _c.scrollIntoView();
-    }
-};
+paintImagesOnScreen(images_to_show);
+addEventListeners();
+/* FUNCTION DEFINITIONS */
+// randomly retrieve desired number of thumbnails from list
 function getRandomThumbnails(arr, num) {
     var result = new Array(num);
     var len = arr.length;
@@ -139,4 +63,65 @@ function getRandomThumbnails(arr, num) {
         taken[x] = --len in taken ? taken[len] : len;
     }
     return result;
+}
+function paintImagesOnScreen(images) {
+    // set number of rows and columns in CSS
+    var gallery = document.getElementById("gallery");
+    gallery.innerHTML = ""; // clear the gallery before painting
+    gallery.style.gridTemplateRows = "repeat(".concat(NUM_ROWS, ", 300px)");
+    gallery.style.gridTemplateColumns = "repeat(".concat(num_cols, ", 400px)");
+    images.forEach(function (filename, index) {
+        // produce the following: <div id="${index}" class="grid-item"><img src="assets/${filename}"></div>
+        var gallery_item = document.createElement("div");
+        gallery_item.id = (index + 1).toString(); // add 1 to the index to process our calculation
+        gallery_item.className = "grid-item";
+        // if this is the first image, set it as active
+        if (index === 0) {
+            gallery_item.className += " active";
+        }
+        var image = document.createElement("img");
+        image.src = "assets/".concat(filename);
+        gallery_item.appendChild(image);
+        gallery.appendChild(gallery_item);
+    });
+}
+function addEventListeners() {
+    window.onkeydown = function (ev) {
+        if (ev.defaultPrevented)
+            return;
+        processKey(ev.key);
+        function processKey(key) {
+            var currentIndex = +document.getElementsByClassName("active")[0].id;
+            var newIndex;
+            if (key === "ArrowUp" && currentIndex - num_cols > 0) {
+                newIndex = currentIndex - num_cols;
+                toggleActive(currentIndex, newIndex);
+            }
+            else if (key === "ArrowDown" && currentIndex + num_cols <= NUM_IMAGES_TO_SHOW) {
+                newIndex = currentIndex + num_cols;
+                toggleActive(currentIndex, newIndex);
+            }
+            else if (key === "ArrowLeft" && currentIndex % num_cols != 1) {
+                newIndex = currentIndex - 1;
+                toggleActive(currentIndex, newIndex);
+            }
+            else if (key === "ArrowRight" && currentIndex % num_cols != 0 && currentIndex + 1 <= NUM_IMAGES_TO_SHOW) {
+                newIndex = currentIndex + 1;
+                toggleActive(currentIndex, newIndex);
+            }
+            else if (key === "r" || key === "End") {
+                // if user presses R or End, refresh page to randomise images again.
+                // we use End to provide convenience to users because it's near the arrow keys on the keyboard.
+                images_to_show = getRandomThumbnails(all_available_images, NUM_IMAGES_TO_SHOW);
+                paintImagesOnScreen(images_to_show);
+                toggleActive(currentIndex, 1);
+            }
+        }
+        function toggleActive(indexToRemove, indexToAdd) {
+            var _a, _b, _c;
+            (_a = document.getElementById(indexToRemove.toString())) === null || _a === void 0 ? void 0 : _a.classList.remove("active");
+            (_b = document.getElementById(indexToAdd.toString())) === null || _b === void 0 ? void 0 : _b.classList.add("active");
+            (_c = document.getElementById(indexToAdd.toString())) === null || _c === void 0 ? void 0 : _c.scrollIntoView();
+        }
+    };
 }
